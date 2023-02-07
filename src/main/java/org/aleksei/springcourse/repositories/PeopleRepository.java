9126f6f29197b11ab3d779eef33cfc5d7926d8b3
AlekseiBriukhachev <abriukhachev@trask.cz>
2023-02-07 23:04:58 +0100

    Initial commit

---
+++ src/main/java/org/aleksei/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package org.aleksei.springcourse.repositories;
+
+import org.aleksei.springcourse.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
---
+++ src/main/java/org/aleksei/springcourse/services/PeopleService.java
@@ -0,0 +1,39 @@
+package org.aleksei.springcourse.services;
+
+import org.aleksei.springcourse.models.Person;
+import org.aleksei.springcourse.repositories.PeopleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+    private final PeopleRepository peopleRepository;
+
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+    public List<Person> findAll(){
+        return peopleRepository.findAll();
+    }
+    public Person findOne(int id){
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+    @Transactional
+    public void save(Person person){
+        peopleRepository.save(person);
+    }
+    @Transactional
+    public void update(int id, Person updatedPerson){
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+    @Transactional
+    public void delete(int id){
+        peopleRepository.deleteById(id);
+    }
+}
